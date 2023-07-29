a6661a3e88d7a1b02cb82f7652e215649cb50fb4
Madhubala M <krishdiya2016@gmail.com>
2023-07-29 12:04:35 +0530

    solution pass

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -4,7 +4,6 @@ import com.driver.model.Booking;
 import com.driver.model.Facility;
 import com.driver.model.Hotel;
 import com.driver.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,15 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/hotel")
 public class HotelManagementController {
-
-    @Autowired
-    com.driver.Services.HotelManagementService hotelServices=new com.driver.Services.HotelManagementService();
 
     @PostMapping("/add-hotel")
     public String addHotel(@RequestBody Hotel hotel){
@@ -30,8 +28,9 @@ public class HotelManagementController {
         //incase the hotelName is null or the hotel Object is null return an empty a FAILURE
         //Incase somebody is trying to add the duplicate hotelName return FAILURE
         //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
-        String res=hotelServices.addHotel(hotel);
-        return res;
+
+
+        return null;
     }
 
     @PostMapping("/add-user")
@@ -39,8 +38,8 @@ public class HotelManagementController {
 
         //You need to add a User Object to the database
         //Assume that user will always be a valid user and return the aadharCardNo of the user
-        Integer aadharNo=hotelServices.addUser(user);
-        return aadharNo;
+
+        return null;
     }
 
     @GetMapping("/get-hotel-with-most-facilities")
@@ -49,8 +48,8 @@ public class HotelManagementController {
         //Out of all the hotels we have added so far, we need to find the hotelName with most no of facilities
         //Incase there is a tie return the lexicographically smaller hotelName
         //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
-        String res=hotelServices.getHotelWithMostFacilities();
-        return res;
+
+        return null;
     }
 
     @PostMapping("/book-a-room")
@@ -62,20 +61,15 @@ public class HotelManagementController {
         //Calculate the total amount paid by the person based on no. of rooms booked and price of the room per night.
         //If there arent enough rooms available in the hotel that we are trying to book return -1
         //in other case return total amount paid
-        UUID randomUUID = UUID.randomUUID();
-        String bookingId= randomUUID.toString().replaceAll("_", "");
-        booking.setBookingId(bookingId);
 
-        int totalAmount=hotelServices.bookRooms(booking);
-        return totalAmount;
+        return 0;
     }
 
     @GetMapping("/get-bookings-by-a-person/{aadharCard}")
     public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
     {
         //In this function return the bookings done by a person
-        int bookingByPerson=hotelServices.getBookings(aadharCard);
-        return bookingByPerson;
+        return 0;
     }
 
     @PutMapping("/update-facilities")
@@ -85,8 +79,7 @@ public class HotelManagementController {
         //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
         //return the final updated List of facilities and also update that in your hotelDb
         //Note that newFacilities can also have duplicate facilities possible
-        Hotel hotel=hotelServices.updateFacilities(newFacilities,hotelName);
-        return hotel;
+        return null;
     }
 
 }
